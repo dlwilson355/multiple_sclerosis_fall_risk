c30dfe5803c2c181cd27dede7de9a01c9c76a099
Jost Eckhardt <jeckhardt@aisquared.com>
2019-03-20 20:01:49 -0400

    minor adjustment

--- recurrentnetwork.py
+++ recurrentnetwork.py
@@ -6,38 +6,10 @@ from keras import backend as K
 #from testdatagenerator import DataGenerator
 from datagenerator import DataGenerator
 
-def Recall(y_true, y_pred):
+def recall(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     all_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     return true_positives / (all_positives + K.epsilon())
-
-def sensitivity(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    return true_positives / (possible_positives + K.epsilon())
-
-def specificity(y_true, y_pred):
-    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
-    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
-    return true_negatives / (possible_negatives + K.epsilon())
-
-
-def f1(y_true, y_pred):
-    def recall(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
-
-    def precision(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + K.epsilon())
-        return precision
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall + K.epsilon())) 
-
 
 class RNNRnunner(object):
     def __init__(self, verb, multi,folder):
@@ -59,8 +31,7 @@ class RNNRnunner(object):
         #opt = keras.optimizers.Adam(lr=lr)
         #opt = keras.optimizers.RMSprop(clipnorm=1., lr=lr)
         opt = keras.optimizers.RMSprop(lr=lr,decay=0.1)
-        #met = [sensitivity, specificity, 'accuracy', f1]
-        met = ['accuracy', sensitivity]
+        met = ['accuracy', recall]
         model.compile(optimizer = opt, loss=lossFct,metrics=met)
         return
 
