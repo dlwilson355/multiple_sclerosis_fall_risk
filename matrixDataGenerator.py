f618f7349d5f92e4e756630eb5235a23eb18503c
Daniel Wilson <dlwilson355@gmail.com>
2019-04-14 13:44:53 -0400

    Performance improvements.

--- matrixDataGenerator.py
+++ matrixDataGenerator.py
@@ -9,15 +9,24 @@ from skimage.transform import resize
 from skimage.color import gray2rgb
 
 class MatrixDataGenerator(keras.utils.Sequence):
-    def __init__(self, directory, matrix_dimensions = "NONE", rgb = False, batch_size=32, grab_data_from = (0, .75), debug = False):
+    def __init__(self, directory, matrix_dimensions = "NONE", rgb = False, batch_size=32, grab_data_from = (0, .75), preload_concatenated_dataframes = True, preload_activity_start_and_end_indicies = True, print_loading_progress = False, debug = False):
         self.master_directory = directory
         self.matrix_dimensions = matrix_dimensions
         self.rgb = rgb
         self.batch_size = batch_size
         self.valid_selection_range = grab_data_from
+        self.print_loading_progress = print_loading_progress
         self.debug = debug
         self.patients = self.get_patient_list()
         self.len = int(np.floor(len(self.patients) / self.batch_size))
+        if (preload_concatenated_dataframes):
+            self.preloaded_concatenated_dataframes = self.preload_concatenated_dataframes()
+        else:
+            self.preloaded_concatenated_dataframes = None
+        if (self.preload_activity_start_and_end_indicies):
+            self.preloaded_activity_start_and_end_indicies = None
+        else:
+            self.preloaded_activity_start_and_end_indicies = self.preload_activity_start_and_end_indicies()
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -42,8 +51,9 @@ class MatrixDataGenerator(keras.utils.Sequence):
         xValues = []
         yValues = []
 
-        self.print_if_debug("Generating batch of data with patients: %s" % (patients))
+        self.print_if_loading("Generating batch of data with patients: %s" % (patients))
         for i, patient in enumerate(patients):
+            self.print_if_loading("Loading patient %d of %d" % (i, len(patients)))
             self.print_if_debug("Generating data for %s" % (patient))
             xData = self.generate_patient_data(patient).values
             xData = tf.keras.utils.normalize(xData, axis=-1)
@@ -55,28 +65,38 @@ class MatrixDataGenerator(keras.utils.Sequence):
             else:
                 xData = xData.reshape((xData.shape[0], xData.shape[1], 1))
                 self.print_if_debug("xData shape after reshaping: %s" % (xData.shape,))
-            one_hot = self.convert_patient_to_one_hot(patient)
+            yData = self.convert_patient_to_one_hot(patient)
+            self.print_if_debug("yData shape: %s" % (yData.shape,))
             xValues.append(xData)
-            yValues.append(one_hot)
+            yValues.append(yData)
         xData = np.array(xValues)
         yData = np.array(yValues)
         return xData, yData
 
     def generate_patient_data(self, patient):
-        (starts, ends) = self.get_test_start_and_end(patient)
-        patient_data = self.get_concatenated_patient_data(patient)
+        self.print_if_debug("Generating data for %s" % (patient))
+        if (self.preloaded_activity_start_and_end_indicies == None):
+            (starts, ends) = self.get_test_start_and_end_indicies(patient)
+        else:
+            (starts, ends) = self.preloaded_activity_start_and_end_indicies[patient]
+        if (self.preloaded_concatenated_dataframes == None):
+            patient_data = self.get_concatenated_patient_data(patient)
+        else:
+            patient_data = self.preloaded_concatenated_dataframes[patient]
         dimension = patient_data.shape[1] * len(starts) # this dimension represents both the length and width of the dataframe
         dataframes = []
         final = pd.DataFrame()
         for i in range(len(starts)):
-            start = self.get_corresponding_index(patient, starts[i])
-            end = self.get_corresponding_index(patient, ends[i])
+            start = starts[i]
+            end = ends[i]
+            self.print_if_debug("Window range from index %d to %d." % (start, end))
             difference = end - start
             valid_start_range = (int(start + difference * self.valid_selection_range[0]), int(start + difference * self.valid_selection_range[1] - dimension))
             if (valid_start_range[1] <= valid_start_range[0]): # need to find a better solution for this, this is necessary if the activity is too short to have a valid range
                 valid_start_range = (valid_start_range[0], valid_start_range[0]+1)
             start_index = random.randint(valid_start_range[0], valid_start_range[1]-1)
             end_index = start_index + dimension
+            self.print_if_debug("Selected window starts from index %d to %d." % (start_index, end_index))
             df = pd.DataFrame(patient_data.iloc[start_index:end_index, ].values)
             dataframes.append(df)
         result = pd.concat(dataframes, axis=1, ignore_index=True)
@@ -94,11 +114,11 @@ class MatrixDataGenerator(keras.utils.Sequence):
         self.print_if_debug(patients)
         return (patients)
 
-    # returns the test start and end timestamps from the corresponding directory
-    def get_test_start_and_end(self, directory):
+    # returns the test start and end timestamps from the corresponding patient directory
+    def get_test_start_and_end_indicies(self, patient):
         starts = []
         ends = []
-        annotations_filepath = os.path.join(directory, "annotations.csv")
+        annotations_filepath = os.path.join(patient, "annotations.csv")
         annotation_data = pd.read_csv(annotations_filepath)
         previous_activity_type = ""
         for row in range(annotation_data.shape[0]):
@@ -106,8 +126,10 @@ class MatrixDataGenerator(keras.utils.Sequence):
             if (not activity_type == previous_activity_type):
                 activity_start_timestamp = annotation_data.iloc[row, 4]
                 activity_end_timestamp = annotation_data.iloc[row, 5]
-                starts.append(activity_start_timestamp)
-                ends.append(activity_end_timestamp)
+                activity_start_index = self.get_corresponding_index(patient, activity_start_timestamp)
+                activity_end_index = self.get_corresponding_index(patient, activity_end_timestamp)
+                starts.append(activity_start_index)
+                ends.append(activity_end_index)
             previous_activity_type = activity_type
         return ((starts, ends))
 
@@ -124,7 +146,7 @@ class MatrixDataGenerator(keras.utils.Sequence):
         for i in range(len(self.patients)):
             if (patient == self.patients[i]):
                 one_hot[i] = 1
-        return (one_hot)
+        return (np.array(one_hot))
 
     # returns a list of patients that no longer contains patients with incomplete data
     def remove_patients_with_incomplete_data(self, patient_list):
@@ -180,6 +202,27 @@ class MatrixDataGenerator(keras.utils.Sequence):
         if (len(str(timestamp)) >= 16):
             return (int(timestamp/1000 + .5))
         return (timestamp)
+
+    # returns a dicitonary with preloaded dataframes with all the sensor readings concatenated vertically for each patient to improve performance
+    def preload_concatenated_dataframes(self):
+        concatenated_dataframes = {}
+        for patient in self.patients:
+            concatenated_dataframes[patient] = self.get_concatenated_patient_data(patient)
+        self.print_if_loading("Preloaded concatenated dataframes.")
+        return (concatenated_dataframes)
+
+    # returns a preloaded dictionary represending the indicies in the concatenated dataframe corresponding to the start and end times of each activity to improve performance
+    def preload_activity_start_and_end_indicies(self):
+        start_end_indicies = {}
+        for patient in self.patients:
+            start_end_indicies[patient] = self.get_test_start_and_end_indicies(patient)
+        self.print_if_loading("Preloaded activity start and end indicies.")
+        return (start_end_indicies)
+
+    # prints out the statement if printing out loading progress (to track perfomance)
+    def print_if_loading(self, string):
+        if (self.print_loading_progress):
+            print(string)
 
     # prints out the statement if dubugging
     def print_if_debug(self, string):
--- matrixDataGeneratorTest.py
+++ matrixDataGeneratorTest.py
@@ -1,12 +1,12 @@
 from keras.applications.resnet50 import ResNet50
 from matrixDataGenerator import MatrixDataGenerator
 
-data_filepath = "D:\\deep learning dataset\\MS Fall Study"
-data_filepath = "data"
+data_filepath = "D:\\deep learning dataset\\MS Fall Study" # you will obviously need to change this to the appropriate filepath based on where you placed the data folder
+#data_filepath = "data"
 
 def create_generators():
-        training_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 200, grab_data_from = (0, .75), debug = False)
-        validation_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 100, grab_data_from = (.75, 1), debug = False)
+        training_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 200, grab_data_from = (0, .75), preload_concatenated_dataframes = True, preload_activity_start_and_end_indicies = True, print_loading_progress = False, debug = False)
+        validation_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 100, grab_data_from = (.75, 1), preload_concatenated_dataframes = True, preload_activity_start_and_end_indicies = True, print_loading_progress = False, debug = False)
         return training_generator, validation_generator
 
 def train_model_with_generator(model, training_generator, validation_generator):
