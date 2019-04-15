e157765d3b97e09919b2c736a28a3c2624802a30
Daniel Wilson <dlwilson355@gmail.com>
2019-04-14 20:37:35 -0400

    Fixed bug where training would not progress if batch size was greater that number of patients.

--- matrixDataGeneratorTest.py
+++ matrixDataGeneratorTest.py
@@ -1,17 +1,18 @@
 from keras.applications.resnet50 import ResNet50
+from keras.applications.vgg16 import VGG16
 from matrixDataGenerator import MatrixDataGenerator
 
 data_filepath = "D:\\deep learning dataset\\MS Fall Study" # you will obviously need to change this to the appropriate filepath based on where you placed the data folder
 #data_filepath = "data"
 
 def create_generators():
-        training_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 200, grab_data_from = (0, .75), preload_concatenated_dataframes = True, preload_activity_start_and_end_indicies = True, print_loading_progress = False, debug = False)
-        validation_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 100, grab_data_from = (.75, 1), preload_concatenated_dataframes = True, preload_activity_start_and_end_indicies = True, print_loading_progress = False, debug = False)
+        training_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 32, grab_data_from = (0, .75), preload_concatenated_dataframes = True, preload_activity_start_and_end_indicies = True, print_loading_progress = False, debug = False)
+        validation_generator = MatrixDataGenerator(directory = data_filepath, matrix_dimensions = (224, 224), rgb = True, batch_size = 32, grab_data_from = (.75, 1), preload_concatenated_dataframes = True, preload_activity_start_and_end_indicies = True, print_loading_progress = False, debug = False)
         return training_generator, validation_generator
 
 def train_model_with_generator(model, training_generator, validation_generator):
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    model.fit_generator(training_generator, epochs=10, validation_data = validation_generator)
+    model.fit_generator(training_generator, epochs=10, steps_per_epoch=20, validation_steps=10, validation_data = validation_generator)
     model.save_weights("resNet_weights.h5")
 
 def main():
