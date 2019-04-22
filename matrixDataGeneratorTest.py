cfd7dbd1204057687793721c9a6e70c40398f04c
Daniel Wilson <dlwilson355@gmail.com>
2019-04-22 17:10:39 -0400

    This version should work.

--- matrixDataGeneratorTest.py
+++ matrixDataGeneratorTest.py
@@ -9,7 +9,7 @@ data_filepath = "data"
 
 def create_generators():
         preLoader = MatrixPreLoader(data_filepath, num_patients_to_use = 15, print_loading_progress = False, debug = False)
-        training_generator = MatrixDataGenerator(preLoader, matrix_dimensions = (224, 224), rgb = True, twoD = False, add_gaussian_noise = None, batch_size = 32, grab_data_from = (0, .75), print_loading_progress = False, debug = False)
+        training_generator = MatrixDataGenerator(preLoader, matrix_dimensions = (224, 224), rgb = True, twoD = False, add_gaussian_noise = .01, batch_size = 32, grab_data_from = (0, .75), print_loading_progress = False, debug = False)
         validation_generator = MatrixDataGenerator(preLoader, matrix_dimensions = (224, 224), rgb = True, twoD = False, add_gaussian_noise = None, batch_size = 32, grab_data_from = (.75, 1), print_loading_progress = False, debug = False)
         return training_generator, validation_generator
 
@@ -18,13 +18,13 @@ def train_model_with_generator(model, training_generator, validation_generator):
         checkpoint = ModelCheckpoint(weights_filepath, monitor="val_acc", verbose=1, save_best_only=True, mode="max")
         optimizer = Adam(lr=0.0001)
         model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-        model.fit_generator(training_generator, epochs=10, steps_per_epoch=20, validation_steps=10, validation_data = validation_generator, callbacks=[checkpoint])
+        model.fit_generator(training_generator, epochs=10, steps_per_epoch=10, validation_steps=10, validation_data=validation_generator, callbacks=[checkpoint])
         model.save_weights("resNet_weights.h5")
 
 def main():
         training_gen, validation_gen = create_generators()
         sample_X, sample_Y = training_gen.__getitem__() # we get sample data because we use it to determine the data dimensions when we create the model
-        model = ResNet50(weights=None, classes = sample_Y.shape[1])
+        model = VGG16(weights=None, classes = sample_Y.shape[1])
         train_model_with_generator(model, training_gen, validation_gen)
 
 if __name__ == "__main__":
